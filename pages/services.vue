acfd16f57581cad2076e94c8228f0d4455357b1d
Lucas <benicio.developer@gmail.com>
2024-08-26 16:33:39 -0300

    pag services initial

--- pages/services.vue
+++ pages/services.vue
@@ -1,6 +1,112 @@
 <template>
     <section class="services">
-        <h1>hellow</h1>
+        <div class="services__business ">
+            <h1>MUDAR A REALIDADE E A <br> CONTABILIDADE DAS EMPRESAS</h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et
+                dolore magna aliqua.<br> Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+                aliquip.</p>
+
+            <div class="services__cards">
+                <div class="services__card">
+                    <div class="services__background">
+                        <NuxtImg src="calculator-icon.png"></NuxtImg>
+                    </div>
+                    <div class="services__information">
+                        <h4>LOREM IPSUM</h4>
+                        <P>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut
+                           
+
+                        </P>
+                    </div>
+
+                </div>
+
+                <div class="services__card">
+                    <div class="services__background">
+                        <NuxtImg src="calculator-icon.png"></NuxtImg>
+                    </div>
+                    <div class="services__information">
+                        <h4>LOREM IPSUM</h4>
+                        <P>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut
+                           
+
+                        </P>
+                    </div>
+
+                </div>
+
+                <div class="services__card">
+                    <div class="services__background">
+                        <NuxtImg src="calculator-icon.png"></NuxtImg>
+                    </div>
+                    <div class="services__information">
+                        <h4>LOREM IPSUM</h4>
+                        <P>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut
+                           
+                        </P>
+                    </div>
+
+                </div>
+
+                
+                
+                
+
+
+                
+            </div>
+
+            <div class="services__cards">
+                <div class="services__card">
+                    <div class="services__background">
+                        <NuxtImg src="calculator-icon.png"></NuxtImg>
+                    </div>
+                    <div class="services__information">
+                        <h4>LOREM IPSUM</h4>
+                        <P>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut
+                           
+
+                        </P>
+                    </div>
+
+                </div>
+
+                <div class="services__card">
+                    <div class="services__background">
+                        <NuxtImg src="calculator-icon.png"></NuxtImg>
+                    </div>
+                    <div class="services__information">
+                        <h4>LOREM IPSUM</h4>
+                        <P>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut
+                           
+
+                        </P>
+                    </div>
+
+                </div>
+
+                <div class="services__card">
+                    <div class="services__background">
+                        <NuxtImg src="calculator-icon.png"></NuxtImg>
+                    </div>
+                    <div class="services__information">
+                        <h4>LOREM IPSUM</h4>
+                        <P>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut
+                           
+                        </P>
+                    </div>
+
+                </div>
+
+                
+                
+                
+
+
+                
+            </div>
+        </div>
+
     </section>
 </template>
 
