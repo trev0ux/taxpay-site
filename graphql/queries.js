c9c0372d842513d1493b13f097a09107b96973eb
Lucas Amorim <lucas.lopes5@hotmail.com>
2024-09-08 15:55:11 -0300

    adjusted fonts queires and team section

--- graphql/queries.js
+++ graphql/queries.js
@@ -6,187 +6,180 @@ export const HOMEPAGE_QUERY = `
       }
     }
     page(id: "home", idType: URI) {
-        title
-        content
+    title
+    content
+    banner {
+      banner1 {
         banner {
-          banner1 {
-            banner {
-               node {
-                id
-                title
-                description
-                mediaItemUrl
-                altText
-              }
-            }
-            backgroundColor
-          }
-          banner2 {
-            banner {
-               node {
-                id
-                title
-                description
-                mediaItemUrl
-                altText
-              }
-            }
-            backgroundColor
-          }
-          banner3 {
-            banner {
-               node {
-                id
-                title
-                description
-                mediaItemUrl
-                altText
-              }
-            }
-            backgroundColor
-          }
-          banner4 {
-            banner {
-               node {
-                id
-                title
-                description
-                mediaItemUrl
-                altText
-              }
-            }
-            backgroundColor
+          node {
+            id
+            title
+            description
+            mediaItemUrl
+            altText
           }
         }
-        cultura {
-          titulo
-          descricao
-          items {
-            cultura1 {
-              titulo
-              imagem {
-                node {
-                  id
-                  mediaItemUrl
-                  altText
-                }
-              }
-            }
-            cultura2 {
-              titulo
-              imagem {
-                node {
-                  id
-                  mediaItemUrl
-                  altText
-                }
-              }
-            }
-            cultura3 {
-              titulo
-              imagem {
-                node {
-                  id
-                 mediaItemUrl
-                 altText
-                }
-              }
-            }
-            cultura4 {
-              titulo
-              imagem {
-                node {
-                  id
-                  mediaItemUrl
-                  altText
-                }
-              }
-            }
+        backgroundColor
+      }
+      banner2 {
+        banner {
+          node {
+            id
+            title
+            description
+            mediaItemUrl
+            altText
           }
         }
-        comoFunciona {
-          titulo
-          itens {
-            accordion1 {
-              titulo
-              descricao
-            }
-            accordion2 {
-              titulo
-              descricao
-            }
-            accordion3 {
-              titulo
-              descricao
-            }
-            accordion4 {
-              titulo
-              descricao
-            }
-          }
-          imagens {
-            imagem1 {
-              node {
-                id
-                mediaItemUrl
-                altText
-              }
-            }
-            imagem2 {
-              node {
-                id
-                mediaItemUrl
-                altText
-              }
-            }
-            imagem3 {
-              node {
-                id
-                mediaItemUrl
-                altText
-              }
-            }
+        backgroundColor
+      }
+      banner3 {
+        banner {
+          node {
+            id
+            title
+            description
+            mediaItemUrl
+            altText
           }
         }
-        sobreNos {
+        backgroundColor
+      }
+      banner4 {
+        banner {
+          node {
+            id
+            title
+            description
+            mediaItemUrl
+            altText
+          }
+        }
+        backgroundColor
+      }
+    }
+    cultura {
+      titulo
+      descricao
+      items {
+        cultura1 {
           titulo
-          descricao
           imagem {
             node {
               id
+              uri
               mediaItemUrl
-              altText
             }
           }
         }
-        timePreview {
+        cultura2 {
           titulo
-          membros {
-            membro1 {
-              titulo
-              linkedin
-              cargo
-              imagem {
-                node {
-                  id
-                  uri
-                }
-              }
-            }
-            membro2 {
-              titulo
-              linkedin
-              cargo
-              imagem {
-                node {
-                  id
-                  uri
-                }
-              }
+          imagem {
+            node {
+              id
+              uri
             }
           }
         }
-     depoimentos {
+        cultura3 {
+          titulo
+          imagem {
+            node {
+              id
+              uri
+            }
+          }
+        }
+        cultura4 {
+          titulo
+          imagem {
+            node {
+              id
+              uri
+            }
+          }
+        }
+      }
+    }
+    comoFunciona {
+      titulo
+      itens {
+        accordion1 {
+          titulo
+          descricao
+        }
+        accordion2 {
+          titulo
+          descricao
+        }
+        accordion3 {
+          titulo
+          descricao
+        }
+        accordion4 {
+          titulo
+          descricao
+        }
+      }
+      imagens {
+        imagem1 {
+          node {
+            id
+            uri
+          }
+        }
+        imagem2 {
+          node {
+            id
+            uri
+          }
+        }
+        imagem3 {
+          node {
+            id
+            uri
+          }
+        }
+      }
+    }
+    sobreNos {
+      titulo
+      descricao
+      imagem {
+        node {
+          id
+          uri
+        }
+      }
+    }
+    timePreview {
+      titulo
+      membros {
+        membro1 {
+          titulo
+          linkedin
+          cargo
+          imagem {
+            node {
+              id
+              uri
+            }
+          }
+        }
+        membro2 {
+          titulo
+          linkedin
+          cargo
+          imagem {
+            node {
+              id
+              uri
+            }
+          }
+        }
+      }
+    }
+    depoimentos {
       titulo
       depoimentos {
         depoimento1 {
--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <banner :banner="banner" v-if="banner"></banner>
+    <banner :banner="banner"></banner>
     <culture :culture="culture" v-if="culture"></culture>
     <how-it-works :how-it-works="howItWorks" v-if="howItWorks"></how-it-works>
      <about-us :content="aboutUs" v-if="aboutUs"></about-us>
@@ -47,14 +47,12 @@ export default {
       try {
         const response = await $axios.post("", { query: HOMEPAGE_QUERY });
         const { data } = response.data;
-        console.log(data.page)
         this.banner = data.page.banner
         this.culture = data.page.cultura;
         this.howItWorks = data.page.comoFunciona;
         this.aboutUs = data.page.sobreNos;
         this.team = data.page.timePreview;
-        this.testimonials = data.page.depoimentos
-
+        this.testimonials = data.page.depoimentos;
       } catch (error) {
         console.error("Error fetching data:", error);
       }
