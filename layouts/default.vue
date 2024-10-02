29c1d47df30dbfa9319fafd898b7e188f81fda2a
Lucas Amorim <lucas.lopes5@hotmail.com>
2024-10-02 12:19:56 -0300

    up

--- layouts/default.vue
+++ layouts/default.vue
@@ -7,8 +7,8 @@
 </template>
 
 <script>
-import Header from "@/components/Header.vue";
-import Footer from "@/components/Footer.vue";
+import Header from "@/components/header.vue";
+import Footer from "@/components/footer.vue";
 
 export default {
   components: { Header, Footer},
